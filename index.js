a0211cbeece9cfd3f3c9db85ef519a81129eb3f1
ra-mariano <ronan.a.mariano@gmail.com>
2022-02-09 00:47:37 -0500

    15/16 passing, working on fixing top cutoff of top dot

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let json=[]
+
 
 const req = new XMLHttpRequest();
 req.open("GET",'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json',true);
@@ -20,21 +20,23 @@ req.onload = function(){
  const w = 900;
  const h = 500;
 
- 
 
 
- const xscale = d3.scaleLinear()
-    .domain([1992,2015])
-    .range([200, w]);
 
-const yscale = d3.scaleTime()
-   .domain(
-    d3.extent(json, function (d) {
-      return d.Time;
-    })
-  )
-   .range([0, h])
+
    
+ const xscale = d3.scaleLinear()
+ .domain([1993,2016])
+ .range([200, w]);
+
+ 
+ const yscale = d3.scaleTime()
+ .domain(
+ d3.extent(json, function (d) {
+   return d.Time;
+ })
+ )
+ .range([0, h])
 
 
 
@@ -42,8 +44,10 @@ const yscale = d3.scaleTime()
  
  const svgarea = d3.select("body")
  .append("svg")
+ .attr("id", "svgarea")
  .attr("width", w+20)
  .attr("height", h+50);
+
 
 svgarea.selectAll("circle")
        .data(json)
@@ -52,14 +56,15 @@ svgarea.selectAll("circle")
        .attr("class","dot")
        .attr("data-yvalue", d => d.Time)
        .attr("data-xvalue", d => d.Year)
-       .attr("r", 4)
+       .attr("r", 5)
        .attr('cx', (d) => xscale(d.Year))
        .attr('cy', (d) => yscale(d.Time))
+       
 
 
 
    
-   const xAxis = d3.axisBottom(xscale);
+            var xAxis = d3.axisBottom(xscale).tickFormat(d3.format("d"));
      
         svgarea.append("g")
            .attr("transform", "translate(00," + h + ")")
@@ -73,6 +78,24 @@ svgarea.selectAll("circle")
            .attr("transform", "translate("+200+", 0)")
            .call(yAxis)
            .attr("id", "y-axis")
+
+
+           var tooltip = d3.select("body")
+           .append("div")
+           .style("position", "absolute")
+           .style("visibility", "hidden")
+           .attr("id","tooltip")
+
+           d3.selectAll(".dot")
+           
+           .on("mouseover", function(){
+           let dataxvalue =  this.getAttribute("data-xvalue")
+           return tooltip
+            .style("visibility", "visible")
+          .html(dataxvalue)})
+
+
+           .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
 
 }
